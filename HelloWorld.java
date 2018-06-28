9cfbfa52db3cfbb1b9d247187698ddb6c03db96f
anusek143 <mail2poornasekhar@gmail.com>
2018-06-28 14:47:04 +0530

    safe

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 
 public static void main(String args[]){
-System.out.println("Hello guys hello Jenkins");
+System.out.println("Hello guys hello Jenkins safe zone");
 
 }
 }
